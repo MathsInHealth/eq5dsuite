99628153098c1b38a1f9cd790a28b7d4d5f2255a
Kim Rand <krand@mathsinhealth.com>
2023-03-15 21:17:30 +0100

    Updated value set functions.

--- R/EQ_functions.R
+++ R/EQ_functions.R
@@ -149,3 +149,291 @@ make_dummies <- function(df, version = "5L", dim.names = c("mo", "sc", "ua", "pd
   if(return_df) tmpout <- as.data.frame(tmpout)
   tmpout
 }
+
+
+#' @title eqvs_add
+#' @description Add user-defined EQ-5D value set and corresponding crosswalk option.
+#' @param df A data.frame or file name pointing to csv file. The contents of the data.frame or csv file should be exactly two columns: state, containing a list of all 3125 (for 5L) or 243 (for 3L) EQ-5D health state vectors, and a column of corresponding utility values, with a suitable name.
+#' @param version Version of the EQ-5D instrument. Can take values 5L (default) or 3L.
+#' @param country Optional string. If not NULL, will be used as a country description for the user-defined value set.
+#' @param description Optional string. If not NULL, will be used as a descriptive text for the user-defined value set. 
+#' @param code2L Optional string. If not NULL, will be used as the two-digit code for the value set. Must be different from any existing national value set code.
+#' @return True/False, indicating success or error.
+#' @examples 
+#' # make nonsense value set
+#' new_df <- data.frame(state = make_all_EQ_indexes(), TEST = runif(3125))
+#' # Add as value set for Fantasia
+#' eqvs_add(new_df, version = "5L", country = 'Fantasia')
+#' @importFrom utils read.csv
+#' @export
+eqvs_add <- function(df, version = "5L", country = NULL, description = NULL, code2L = NULL, code3L = NULL, three_letter_code = NULL) {
+  pkgenv <- getOption("eq.env")
+  if(class(df) == 'character') {
+    if(!file.exists(df)) stop('File named ', df, ' does not appear to exist. Exiting.')
+    df <- utils::read.csv(file = df, stringsAsFactors = F)
+  } 
+  if(!NCOL(df) == 2) stop('df should have exactly two columns.')
+  class(df[, 1]) <- 'integer'
+  
+  # read off version-dependent parameters
+  j <- if (version == "5L") 5 else 3
+  states_str <- paste0("states_", version)
+  eq5d_str <- paste0("EQ-5D-", version)
+  uservsets_str <- paste0("uservsets", version)
+  user_defined_str <- paste0("user_defined_", version)
+  
+  # check correct number of rows
+  n <- j^5
+  if(!NROW(df) == n) stop(paste0("df should have exactly ", n, " rows."))
+  # message(NROW(pkgenv$states_5L))
+  # check all states present
+  if(!all(df[,1] %in% pkgenv[[states_str]]$state)) stop(paste0("First column of df should contain all ", eq5d_str, " health state indexes exactly once."))
+  df <- df[match(pkgenv[[states_str]]$state, df[,1]),]
+  if(!is.null(country)) {
+    if(length(country)>1) {
+      warning('Length of country argument > 1, first item used.')
+      country <- country[1]
+    }
+  }
+  if(!is.null(description)) {
+    if(length(description)>1) {
+      warning('Length of description argument > 1, first item used.')
+      description <- description[1]
+    }
+  }
+  thisName <- colnames(df)[2]
+  if(thisName %in% colnames(pkgenv[[uservsets_str]])) {
+    warning(paste0("New country name already in user-defined ", eq5d_str, " value set list."))
+    return(0)
+  }
+  
+  if(any(is.na(df[,2]*-1.1))) stop("Non-numeric values in second column of df.")
+  # No problems
+  tmp <- pkgenv[[uservsets_str]]
+  tmp[, thisName] <- df[, 2]
+  
+  assign(x = uservsets_str, value = tmp, envir = pkgenv)
+  
+  tmp <- data.frame(Name = ifelse(is.null(description), NA, description), 
+                    Name_short = ifelse(is.null(country), NA, country), 
+                    ISO3166Alpha2 = colnames(df)[2], 
+                    ISO3166Alpha3 = NA)
+  
+  if(user_defined_str %in% names(pkgenv)) tmp <- rbind(pkgenv[[user_defined_str]], tmp)
+  
+  assign(x = user_defined_str, value = tmp, envir = pkgenv)
+  
+  # assign(x = 'xwsets', value = pkgenv$probs %*% cbind(as.matrix(.vsets5L[, -1, drop = F]), as.matrix(pkgenv$uservsets5L[,-1, drop = F])), envir = pkgenv)
+  
+  yesno <- 'n'
+  if(file.exists(file.path(pkgenv$cache_path, "cache.Rdta"))) {
+    save(list = ls(envir = pkgenv), envir = pkgenv, file = file.path(pkgenv$cache_path, 'cache.Rdta'))
+    message('User-defined value set saved.')
+  } else {
+    message('If you wish for the user-defined reverse value set to be persistent, a file needs to be saved to the hard-drive.')
+    message('The file would be saved to: ', pkgenv$cache_path)
+    message('Please type "y", "Y", "Yes", or "yes" to allow this file to be saved. All other inputs will be interpreted as "no".')
+    yesno = readline(prompt = "Save? (Yes/No) : ")  
+    if(tolower(yesno) %in% c("yes", "y")) {
+      message('Persistent data will be saved.')
+      if(!dir.exists(pkgenv$cache_path)) dir.create2(pkgenv$cache_path, recursive = TRUE)
+      save(list = ls(envir = pkgenv), envir = pkgenv, file = file.path(pkgenv$cache_path, 'cache.Rdta'))
+      message('User-defined value set saved.')
+      
+    } else {
+      message('Persistent data will not be saved; any user-defined value sets will disappear when this session is closed.')
+    }
+  }
+  
+  .fixPkgEnv()
+  1
+}
+
+#' @title eqvs_drop
+#' @description Drop user-defined EQ-5D value set to reverse crosswalk options.
+#' @param version Version of the EQ-5D instrument. Can take values 5L (default) or 3L.
+#' @param country Optional string. If NULL, a list of current user-defined value sets will be provided for selection. If set, and matching an existing user-defined value set, a prompt will be given as to whether the value set should be deleted. 
+#' @return True/False, indicating success or error.
+#' @examples 
+#' # make nonsense value set
+#' new_df <- data.frame(state = make_all_EQ_indexes(), TEST = runif(3125))
+#' # Add as value set for Fantasia
+#' eqvs_add(new_df, version = "5L", country = 'Fantasia')
+#' # Drop value set for Fantasia
+#' eqvs_drop('Fantasia')
+#' @export
+eqvs_drop <- function(country = NULL, version = "5L") {
+  pkgenv <- getOption("eq.env")
+  if(length(country)>1) {
+    message('Length of country argument larger than 1, only the first element will be used.')
+    country <- country[[1]]
+  }
+  
+  # read off version-dependent parameters
+  version <- toupper(version)
+  user_defined_str <- paste0("user_defined_", version)
+  uservsets_str <- paste0("uservsets", version)
+  
+  states_str <- paste0("states_", version)
+  eq5d_str <- paste0("EQ-5D-", version)
+  
+  
+  
+  if(!user_defined_str %in% names(pkgenv)) {
+    message(paste0("No user-defined value sets exist among the ", version, " datasets. Exiting."))
+    return(FALSE)
+  } else {
+    udc <- pkgenv[[user_defined_str]]
+    if(!is.null(country)) {
+      tmp <- which(toupper(as.matrix(udc)) == toupper(country), arr.ind = T)
+      
+      if(nrow(tmp)) {
+        country <- udc$ISO3166Alpha2[tmp[1,1]]
+        yesno = readline(prompt = paste0('Are you sure you want to delete user-defined country "', country,'" from the ', version, 'value sets? ([Y]es/[N]o) : '))
+        if(tolower(yesno) %in% c("yes", "y")) {
+          message('Removing ', country, ' from user-defined value sets.')
+          udc <- udc[-tmp[1,1],]
+          if(NROW(udc)) {
+            assign(x = user_defined_str, value = udc, envir = pkgenv)
+          } else {
+            rm(list = user_defined_str, envir = pkgenv)
+          }
+          tmp <- pkgenv[[uservsets_str]]
+          tmp[, which(colnames(tmp) == country)] <- NULL
+          assign(x = uservsets_str, value = tmp, envir = pkgenv)
+          .fixPkgEnv()
+          return(TRUE)
+        } else {
+          message("OK. Exiting.")
+          return(FALSE)
+        }
+      } else {
+        message('User-defined value set identified as ', country, ' not found among the ', version, ' value sets.')
+      }
+    }
+  }
+  
+  udc <- udc[, 3:1]
+  colnames(udc) <- c('Code', 'Name', 'Description')
+  message('Please enter which value set you wish to drop, or enter any other value to exit.')
+  .prettyPrint(udc)
+  yesno = trimws(toupper(readline(prompt = paste0('Please enter value set name, description, or code: '))))
+  tmp <- which(toupper(as.matrix(udc)) == yesno, arr.ind = T)
+  if(NROW(tmp)) return(eqvs_drop(tmp), version)
+  message('Exiting.')
+  return(FALSE)
+}
+
+
+
+#' @title eqvs_display
+#' @description Display available value sets, which can also be used as (reverse) crosswalks.
+#' @param return_df If set to TRUE, the function will return information on the names of the available value sets in a data.frame. Defaults to FALSE
+#' @param version Version of the EQ-5D instrument. Can take values 5L (default) or 3L.
+#' @return Default NULL, if return_df == TRUE, returns a data.frame with the displayed information.
+#' @examples 
+#' # Display available value sets.
+#' eqvs_display
+#' @export
+eqvs_display <- function(version = "5L", return_df = FALSE) {
+  pkgenv <- getOption("eq.env")
+  
+  # read off version name
+  version <- toupper(version)
+  user_defined_str <- paste0("user_defined_", version)
+  
+  message(str_c("Available national value sets for ", version, " version:"))
+  .prettyPrint(pkgenv$country_codes[[version]], 'l')
+  if(NROW(pkgenv[[user_defined_str]])) {
+    message('User-defined value sets:')    
+    .prettyPrint(pkgenv[[user_defined_str]], 'l')
+  } else {
+    message('No user-defined value sets available.')
+  }
+  if(return_df) 
+    return(rbind(cbind(Type = 'Value set', pkgenv$country_codes[[version]]), if(NROW(pkgenv[[user_defined_str]])) cbind(Type = 'User-defined', pkgenv[[user_defined_str]]) else NULL))
+  retval <- NULL
+}
+
+
+#' @title eq5d
+#' @description Get EQ-5D index values for the -3L, -5L, crosswalk (-3L value set applied to -5L health states),  reverse crosswealk (-5L value set applied to -3L health states), and -Y-3L
+#' @param x A vector of 5-digit EQ-5D-3L state indexes or a matrix/data.frame with columns corresponding to EQ-5D state dimensions
+#' @param version String indicating which version to use. Options are '5L'  (default), '3L', 'xw', 'xwr', and 'Y3L'
+#' @param country String vector indicating country name(s) or country code(s) according to ISO3166 Alpha 2 or Alpha 3
+#' @param dim.names A vector of dimension names to identify dimension columns
+#' @return A vector of values or data.frame with one column for each value set requested.
+#' @examples 
+#' eq5d(c(11111, 12321, 32123, 33333), 'US', '3L') # US -3L value set
+#' eq5d(make_all_EQ_states('3L'), c('DK', 'US'), 'XWR') # Danish and US -5L value sets applied to -3L descriptives, i.e. reverse crosswalk
+#' eq5d(c(11111, 12321, 32153, 55555), 'US', '5L') # US -5L value set
+#' @export
+eq5d <- function(x, country = NULL, version = '5L', dim.names = c("mo", "sc", "ua", "pd", "ad")) {
+  pkgenv <- getOption("eq.env")
+  version <- toupper(version)
+  if(!version %in% c('3L', '5L', 'Y3L', 'XW', 'RXW', 'XWR', 'CW', 'CWR', 'RCW')) stop("No valid argument for eq-5d version.")
+  version <- c('3L', '5L', 'Y3L', 'XW', 'XWR', 'XWR', 'XW', 'XWR', 'XWR')[match(version, c('3L', '5L', 'Y3L', 'XW', 'RXW', 'XWR', 'CW', 'CWR', 'RCW'))]
+  
+  vers <- c('3L', '5L', 'Y3L', '3L', '5L')[match(x = version, table = c('3L', '5L', 'Y3L', 'XW', 'XWR'))]
+  
+  if(!length(dim.names) == 5) stop("Argument dim.names not of length 5.")
+  if(length(dim(x)) == 2) {
+    colnames(x) <- tolower(colnames(x))
+    if(is.null(colnames(x))) {
+      message("No column names")
+      if(NCOL(x) == 5) {
+        message("Given 5 columns, will assume dimensions in conventional order: MO, SC, UA, PD, AD.")
+        colnames(x) <- dim.names
+      } 
+    }
+    if(!all(dim.names %in% colnames(x))) stop("Provided dimension names not available in matrix/data.frame.")
+    x <- toEQ5Dindex(x = x, dim.names = dim.names)
+  }
+  
+  country <- .fixCountries(country, EQvariant = vers)
+  if(any(is.na(country))) {
+    isnas <- which(is.na(country))
+    for(i in isnas)  warning('Country ', names(country)[i], ' not found. Dropped.')
+    country <- country[!is.na(country)]
+  }
+  
+  if(length(country)==0) {
+    message('No valid countries listed. These value sets are currently available.')
+    eqvs_display(version = vers)
+    stop('No valid countries listed.')
+  }
+
+  
+  if(length(country)>1) {
+    names(country) <- country
+    return(do.call(cbind, lapply(country, function(count) eq5d(x, count, version, dim.names))))
+  }
+  
+  xorig <- x
+  x <- as.integer(x)
+  if(version %in% c('3L', 'Y3L', 'XWR')) {
+    x[!regexpr("^[1-3]{5}$", x)==1] <- NA
+  } else {
+    x[!regexpr("^[1-5]{5}$", x)==1] <- NA
+  } 
+  
+  vset <- switch(EXPR = version,
+                 '3L' = pkgenv$vsets3L_combined,
+                 '5L' = pkgenv$vsets5L_combined,
+                 'XW' = pkgenv$xwsets,
+                 'XWR' = pkgenv$xwrsets,
+                 'Y3L' = pkgenv$vsetsY3L)
+  svec <- switch(EXPR = version,
+                 '3L' = pkgenv$states_3L,
+                 '5L' = pkgenv$states_5L,
+                 'XW' = pkgenv$states_5L,
+                 'XWR' = pkgenv$states_3L,
+                 'Y3L' = pkgenv$states_3L)
+  
+  xout <- rep(NA, length(x))
+  
+  xout[!is.na(x)] <- vset[match(x[!is.na(x)], svec$state), country]
+  names(xout) <- xorig
+  xout
+  
+}
--- R/eqxw.R
+++ R/eqxw.R
@@ -2,31 +2,15 @@
 #' @description Takes a 15 x 5 matrix with probabilities per level/dimension, and creates an 3125x243 matrix with probabilities per state
 #' @param .EQxwprob 15 x 5 matrix with probabilities per level/dimension, typically saved in .EQxwprob
 #' @return An 3125x243 matrix with probabilities per state
-.pstate5t3 <- function(.EQxwprob) {
+.pstate5t3 <-  function(probs = .EQxwprob) {
   
-  probs5t3 <- matrix(nrow = 5^5, ncol = 3^5)
+  allst5l <- make_all_EQ_states()
+  allst3l <- make_all_EQ_states(version = '3L')
   
-  for (a1 in 1:5)
-    for (b1 in 1:3) {
-      p1 <- .EQxwprob[b1, a1]
-      for (a2 in 1:5)
-        for (b2 in 1:3) {
-          p2 <- .EQxwprob[b2 + 3, a2]
-          for (a3 in 1:5)
-            for (b3 in 1:3) {
-              p3 <- .EQxwprob[b3 + 6, a3]
-              for (a4 in 1:5)
-                for (b4 in 1:3) {
-                  p4 <- .EQxwprob[b4 + 9, a4]
-                  for (a5 in 1:5)
-                    for (b5 in 1:3) {
-                      p5 <- .EQxwprob[b5 + 12, a5]
-                      i <- a5 + 5*(a4-1) + 25*(a3-1) + 125*(a2-1) + 625*(a1-1)
-                      j <- b5 + 3*(b4-1) + 9*(b3-1) + 27*(b2-1) + 81*(b1-1)
-                      probs5t3[i, j] <- p1*p2*p3*p4*p5
-                    }}}}}
-  return(probs5t3)
+  t(Reduce('*', lapply(0:4, function(i)    probs[i*3+allst3l[, i+1],allst5l[,i+1]])))
 }
+
+
 
 #' @title eqxw
 #' @description Get crosswalk values
@@ -39,45 +23,5 @@
 #' eqxw(make_all_EQ_states('5L'), c('DK', 'US'))
 #' @export
 eqxw <- function(x, country = NULL, dim.names = c("mo", "sc", "ua", "pd", "ad")) {
-  pkgenv <- getOption("eq.env")
-  if(!length(dim.names) == 5) stop("Argument dim.names not of length 5.")
-  if(length(dim(x)) == 2) {
-    colnames(x) <- tolower(colnames(x))
-    if(is.null(colnames(x))) {
-      message("No column names")
-      if(NCOL(x) == 5) {
-        message("Given 5 columns, will assume dimensions in conventional order: MO, SC, UA, PD, AD.")
-        colnames(x) <- dim.names
-      } 
-    }
-    if(!all(dim.names %in% colnames(x))) stop("Provided dimension names not available in matrix/data.frame.")
-    x <- toEQ5Dindex(x = x, dim.names = dim.names)
-  }
-  
-  country <- .fixCountries(country, EQvariant = '3L')
-  if(any(is.na(country))) {
-    isnas <- which(is.na(country))
-    for(i in isnas)  warning('Country ', names(country)[i], ' not found. Dropped.')
-    country <- country[!is.na(country)]
-  }
-  
-  if(length(country)==0) {
-    message('No valid countries listed. These value sets are currently available.')
-    eqxwr_display()
-    stop('No valid countries listed.')
-  }
-  
-  x <- as.integer(x)
-  x[!regexpr("^[1-5]{5}$", x)==1] <- NA
-  
-  if(length(country)>1) {
-    names(country) <- country
-    return(do.call(cbind, lapply(country, function(count) eqxw(x, count, dim.names))))
-  }
-  
-  xout <- rep(NA, length(x))
-  
-  xout[!is.na(x)] <- pkgenv$xwsets[match(x[!is.na(x)], pkgenv$states_5L$state), country]
-  xout
-  
+  do.call(eq5d, c(as.list(match.call())[-1],version = "xw"))
 }
